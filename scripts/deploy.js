99805ecd4f51f35a0492559e948a3439fa50964f
dawlau <blahoviciandrei1@gmail.com>
2022-04-21 22:42:48 +0300

    wip

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,17 +1,12 @@
-async function main() {
+async function deploy() {
 	const Ticket = await ethers.getContractFactory("Ticket")
 	const ticket = await Ticket.deploy()
 	await ticket.deployed()
-	// const TicketList = await ethers.getContractFactory("TicketsList")
-	// const ticketList = await TicketList.deploy(ticket.address)
-	// await ticketList.deployed()
+	const TicketList = await ethers.getContractFactory("TicketsList")
+	const ticketList = await TicketList.deploy(ticket.address)
+	await ticketList.deployed()
 	console.log("Contract deployed to address:", ticket.address)
-	// console.log("Contract deployed to address:", ticketList.address)
+	console.log("Contract deployed to address:", ticketList.address)
 }
 
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error)
-    process.exit(1)
-  })
+deploy()
